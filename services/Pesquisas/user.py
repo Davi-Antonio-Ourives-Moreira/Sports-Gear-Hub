8fc86257b64ac070820194994c38a9de1130da44
Davi-Antonio-Ourives-Moreira <daviaomoreira@gmail.com>
2024-07-27 12:35:07 -0300

    sistema de carrinho nas pesquisas

--- services/Pesquisas/user.py
+++ services/Pesquisas/user.py
@@ -6,9 +6,13 @@ class Pesquisas(object):
     def __init__(self, nome_produto) -> None:
         self.banco_dados_carrinho = Banco_Dados_Carrinho()
 
-        self.verificar_quantidade_produtos_carrinho = self.banco_dados_carrinho.Dados_Produtos_Carrinho()
+        self.verificar_produtos_carrinho = self.banco_dados_carrinho.Dados_Produtos_Carrinho()
 
-        self.verificar_quantidade_produtos_carrinho = len(self.verificar_quantidade_produtos_carrinho)
+        self.verificar_quantidade_produtos_carrinho = len(self.verificar_produtos_carrinho)
+
+        self.produtos_carrinho = []
+
+        self.preco_total_produtos_carrinho = 0
 
         self.nome_produto = nome_produto
 
@@ -22,7 +26,15 @@ class Pesquisas(object):
         for produto_pesquisado in self.pesquisa:
             self.informacoes_produtos_pesquisados.append((produto_pesquisado[0], produto_pesquisado[1], produto_pesquisado[2], produto_pesquisado[3]))
 
+        if self.verificar_quantidade_produtos_carrinho > 0:
+            for produto_carrinho in self.verificar_produtos_carrinho:
+                self.produtos_carrinho.append((produto_carrinho[0], produto_carrinho[1], produto_carrinho[2], produto_carrinho[3]))
+
+                self.preco_total_produtos_carrinho += float(produto_carrinho[1])
+
         return flask.render_template("pesquisas.html",
                                      produto_pesquisados=self.informacoes_produtos_pesquisados,
                                      quantidade_produtos_carrinho=self.verificar_quantidade_produtos_carrinho,
-                                     nome_pesquisa=self.nome_produto)
+                                     nome_pesquisa=self.nome_produto,
+                                    todos_produtos_carrinho=self.produtos_carrinho,
+                                    preco_total_produtos_carrinho=round(self.preco_total_produtos_carrinho, 2))
