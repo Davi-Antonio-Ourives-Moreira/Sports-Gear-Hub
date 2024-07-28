8fa4e4c756f35796419a6833bfc7d30c7ffd04aa
Davi-Antonio-Ourives-Moreira <daviaomoreira@gmail.com>
2024-07-28 15:12:43 -0300

    js da pagina de verificacao de pagamento

---
+++ controller/static/js/animacao_aviso_pagamento.js
@@ -0,0 +1,48 @@
+
+window.onload = function() {
+    
+    document.querySelector('.icon-container').style.opacity = '1';
+    document.querySelector('.message').style.opacity = '1';
+};
+
+
+window.onload = function() {
+    
+    const xIcon = document.querySelector('.x-icon');
+
+    setTimeout(() => {
+        xIcon.style.opacity = '1';
+    }, 1000); 
+};
+
+
+window.onload = function() {
+    
+    const countdownTime = 0.1 * 60; 
+    let timeRemaining = countdownTime;
+
+    const timerElement = document.getElementById('timer');
+
+    function updateTimer() {
+        const minutes = Math.floor(timeRemaining / 60);
+        const seconds = timeRemaining % 60;
+        timerElement.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    }
+
+    function countdown() {
+        if (timeRemaining <= 0) {
+            
+            window.location.href = 'http://127.0.0.1:3030/'; 
+            return;
+        }
+
+        timeRemaining--;
+        updateTimer();
+        setTimeout(countdown, 1000); 
+    }
+
+    
+    updateTimer();
+    countdown();
+};
+
