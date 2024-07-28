1b7b38abf687fcaece9fcb8434039c210263dc3c
Davi-Antonio-Ourives-Moreira <daviaomoreira@gmail.com>
2024-07-28 15:08:33 -0300

    servico de pagamento

---
+++ services/Pagamento/user.py
@@ -0,0 +1,33 @@
+import flask
+import stripe
+
+class Pagamento(object):
+    def __init__(self, produtos, preco_total_produtos) -> None:
+        self.produtos = produtos
+        self.preco_total_produtos = preco_total_produtos
+
+        stripe.api_key = "sk_test_51PhWh5Bh7PZmW4kqG6hAxJ1ZWf0XMfOnXxV6qfsXIWgVqe0PQDfSAxU4QMRFhnFLNae1dxn6VT6916YatLIY80gA00Kn840RyN"
+
+    
+    def Pagar(self):
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[{
+                'price_data': {
+                    'currency': 'brl',
+                    'product_data': {
+                        'name': self.produtos,
+                    },
+                    'unit_amount': int(self.preco_total_produtos * 100),  # em centavos
+                },
+                'quantity': 1,
+            }],
+            mode='payment',
+            success_url="http://127.0.0.1:3030/pagamento-efetuado-sucesso",
+            cancel_url="http://127.0.0.1:3030/pagamento-cancelado",
+        )
+
+        return flask.redirect(session.url)
+
+
+
